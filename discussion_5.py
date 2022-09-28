9c41f42e75fa2e4c6658394a0278bd03b85a114f
jasonkem <114004881+jasonkem@users.noreply.github.com>
2022-09-28 18:52:49 -0400

    Add files via upload
    commit 1

--- discussion_5.py
+++ discussion_5.py
@@ -1,13 +1,19 @@
 import unittest
 
+
 # Counts the number of a's in a sentence (e.g., a string)
 def count_a(sentence):
 	total = 0
-	for i in range(len(sentence) - 1):
-		if i == 'a':
+	for i in range(len(sentence)):
+		if sentence[i] == 'a':
 			total += 1
 	return total
 
+class myTests(unittest.TestCase):
+
+    def test_checkValue(self):
+        self.assertEqual(count_a("Jason"), 1, "false")
+        self.assertEqual(count_a("aaaa"), 4, 'false')
 
 # Item class
 # Describes an item to be sold. Each item has a name, a price, and a stock.
@@ -38,7 +44,8 @@ class Warehouse:
 
 	# Adds an item to the warehouse	
 	def add_item(self, item):
-		pass
+		self.items.append(item)
+
 
 	# Returns the item in the warehouse with the most stock		
 	def get_max_stock(self):
@@ -53,32 +60,32 @@ class Warehouse:
 # Tests
 class TestAllMethods(unittest.TestCase):
 
-	# SetUp -- we create a bunch of items for you to use in your tests.
-	def setUp(self):
-		self.item1 = Item("Beer", 6, 20)
-		self.item2 = Item("Cider", 5, 25)
-		self.item3 = Item("Water", 1, 100)
-		self.item4 = Item("Fanta", 2, 60)
-		self.item5 = Item("CocaCola", 3, 40)
+# 	# SetUp -- we create a bunch of items for you to use in your tests.
+# 	def setUp(self):
+# 		self.item1 = Item("Beer", 6, 20)
+# 		self.item2 = Item("Cider", 5, 25)
+# 		self.item3 = Item("Water", 1, 100)
+# 		self.item4 = Item("Fanta", 2, 60)
+# 		self.item5 = Item("CocaCola", 3, 40)
 
 	## Check to see whether count_a works
 	def test_count_a(self):
 		pass
 
 
-	## Check to see whether you can add an item to the warehouse
-	def test_add_item(self):
-		pass
+# 	## Check to see whether you can add an item to the warehouse
+# 	def test_add_item(self):
+# 		pass
 
 
-	## Check to see whether warehouse correctly returns the item with the most stock
-	def test_warehouse_max_stocks(self):
-		pass
+# 	## Check to see whether warehouse correctly returns the item with the most stock
+# 	def test_warehouse_max_stocks(self):
+# 		pass
 
 
-	# Check to see whether the warehouse correctly return the item with the highest price
-	def test_warehouse_max_price(self):
-		pass
+# 	# Check to see whether the warehouse correctly return the item with the highest price
+# 	def test_warehouse_max_price(self):
+# 		pass
 		
 
 def main():
